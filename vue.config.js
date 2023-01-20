d0478cca26baab709bb443cfa6869241d4daf132
kyamadwin <koeggjj@gmail.com>
2023-01-20 23:22:18 +0900

    configいじってる

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
     plugins: [
       new NodePolyfillPlugin(),
-      new MyAwesomeWebpackPlugin()
+      // new MyAwesomeWebpackPlugin()
     ],
     optimization: {
       splitChunks: {
