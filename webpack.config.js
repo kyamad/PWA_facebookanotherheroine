9ae1c3356dcaf4a56f29ca3cd2a19b8df1ecef60
kyamadmac <koeggjj@gmail.com>
2023-01-20 19:06:00 +0900

    ビルド環境構築中

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,83 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 // https://qiita.com/itachi/items/0910e5eda52779ffc2c0
 
-const path = require("path");
+const path = require("path")
+const {VueLoaderPlugin } = require("vue-loader")
 
-const isProduction = process.env.NODE_ENV == "production";
+module.exports = {
+    //モードをdevelopment、production、noneから設定(必須)
+    //development: 開発時のファイル出力モード(最適化より時間短縮、エラー表示を優先）
+    //production: 本番時のファイル出力モード（最適化されて出力）
+    mode:'production',
 
-const stylesHandler = "style-loader";
-
-const config = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+    //メインとなるjsファイル(エントリーポイント)
+    entry: "./src/main.ts",
+    //ファイルの出力設定
+    output: {
+        //出力先のディレクトリ（絶対パスで指定）
+        path: path.resolve(__dirname, "dist"),
+        //出力ファイル名
+        filename: "bundle.js",
+    },
+    //ローダーの設定
+    module: {
+        rules: [
+            {
+                test: /\.css$/, //拡張子が.cssの場合
+                use: ["vue-style-loader", "css-loader"] //vue-style-loader、css-loaderを使う
+            },
+            {
+                test:/\.vue$/, //拡張子が.vueの場合
+                loader: "vue-loader" //vue-loaderを使う
+            },
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            }
+        ]
+    },
+    plugins: [
+        //Vueを読み込むために必要
+        new VueLoaderPlugin()
     ],
-  },
-};
+}
 
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
-};
+// const path = require("path");
+// const isProduction = process.env.NODE_ENV == "production";
+// const stylesHandler = "style-loader";
+
+// const config = {
+//   entry: "./src/main.ts",
+//   output: {
+//     path: path.resolve(__dirname, "dist"),
+//   },
+//   plugins: [
+//     // Add your plugins here
+//     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ["vue-style-loader", "css-loader"]
+//       },
+//       {
+//         test: /\.vue$/,
+//         loader: "vue-loader"
+//       },
+      
+
+//       // Add your rules for custom modules here
+//       // Learn more about loaders from https://webpack.js.org/loaders/
+//     ],
+//   },
+// };
+
+// module.exports = () => {
+//   if (isProduction) {
+//     config.mode = "production";
+//   } else {
+//     config.mode = "development";
+//   }
+//   return config;
+// };
