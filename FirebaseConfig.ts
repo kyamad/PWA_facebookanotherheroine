a10ab4d0b8ff46818b61f0dc750f18f85c9d3a03
kyamadwin <koeggjj@gmail.com>
2023-01-22 23:45:06 +0900

    Firebase実装

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,4 +1,3 @@
-console.log("Hello World!");
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -10,6 +9,7 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyA5CbHVOg2wOadrVm0uR6T16-1byLDpGKw",
   authDomain: "amateras-952e1.firebaseapp.com",
+  databaseURL: "https://amateras-952e1-default-rtdb.firebaseio.com",
   projectId: "amateras-952e1",
   storageBucket: "amateras-952e1.appspot.com",
   messagingSenderId: "35101112851",
@@ -19,4 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);ß
+const analytics = getAnalytics(app);
