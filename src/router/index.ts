5bcd5a045cef23c8cae278a9321c010a307fb00d
kyamadmac <koeggjj@gmail.com>
2023-01-27 11:20:41 +0900

    Win移動用

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import StreamingView from '../views/StreamingView.vue'
 import LiveStreaming from '../views/LiveStreaming.vue'
 import index from '../views/index.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -24,6 +25,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/streaming',
     name: 'streaming',
     component: LiveStreaming
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
   },
   {
     path: '/about',
