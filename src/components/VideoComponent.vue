528d103059738fdee9201123f0e36d046378b0b5
kyamadwin <koeggjj@gmail.com>
2023-02-27 17:52:40 +0900

    wp CameraTest MacでAgoraのカメラプレビューをするためのCommit

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="filter" v-if="!Start"></div>
   <button type="button" v-if="!Start" id="startbtn" @click="join()">LIVE<br>スタート</button>
-  <div v-if="Start" id="local_video"></div>
+  <div id="local_video"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import AgoraRTC, { IAgoraRTCClient } from "agora-rtc-sdk-ng";
-import { RtcTokenBuilder, RtcRole } from 'agora-access-token';
 import { onMounted } from 'vue';
 import AgoraLiveStreaming from '../services/AgoraFunctions'
-
-declare var document:any; // documentオブジェクトの型チェックエラー一時海回避用
 
 export default defineComponent({
   name: 'VideoComponent',
@@ -22,48 +18,14 @@ export default defineComponent({
   },
   methods: {
     join: function() {
-
       this.Start = true as boolean ;
       AgoraLiveStreaming.VideoStreaming();
-      
-      // let rtc:any = {
-      //   localAudioTrack: null,
-      //   localVideoTrack: null,
-      //   client: null
-      // };
-      
-      // let options = {
-      //   appId: "49d72a2fc8dc4917804e9e8bacde2661",
-      //   channel: "haisinnmeiireru",
-      //   uid: "aaaaa",
-      //   appCertificate: "e4736ccd47ad4fb9ae0bc8e713398b55",
-      //   expirationTimeInSeconds : 60 * 60,// トークンの有効期間
-      //   currentTimestamp : Math.floor(Date.now() / 1000)
-      // };
-
-      // const privilegeExpiredTs = options.currentTimestamp + options.expirationTimeInSeconds;
-      // const token = RtcTokenBuilder.buildTokenWithAccount(options.appId, options.appCertificate, options.channel, options.uid, RtcRole.PUBLISHER, privilegeExpiredTs);
-      
-      // async function initializeRTC() {
-      //   rtc.client = AgoraRTC.createClient({ mode: "live", codec: "vp8" }) as IAgoraRTCClient;
-      //   await rtc.client.join(options.appId, options.channel, token, options.uid);
-      //   rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-      //   rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
-      //   rtc.localVideoTrack.play("local_video");
-      //   await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
-      // }
-      // initializeRTC();
     }
   },
 
   setup(){
-    onMounted(() => {
-      let rtc:any = {
-        localAudioTrack: null,
-        localVideoTrack: null,
-        client: null
-      };
-      
+    onMounted(() => {  
+      AgoraLiveStreaming.VideoLocalPreview();    
     });
   }
 })
--- src/services/AgoraFunctions.ts
+++ src/services/AgoraFunctions.ts
@@ -2,35 +2,70 @@ import AgoraRTC, { IAgoraRTCClient } from "agora-rtc-sdk-ng";
 import { RtcTokenBuilder, RtcRole } from 'agora-access-token';
 
 class AgoraLiveStreaming {
-    VideoStreaming(){
-        let rtc:any = {
-            localAudioTrack: null,
-            localVideoTrack: null,
-            client: null
-          };
-          
-          let options = {
+      
+      public options:any = {};
+      public rtc:any = {};
+
+      constructor(){
+        this.options = {
             appId: "49d72a2fc8dc4917804e9e8bacde2661",
             channel: "haisinnmeiireru",
             uid: "aaaaa",
             appCertificate: "e4736ccd47ad4fb9ae0bc8e713398b55",
             expirationTimeInSeconds : 60 * 60,// トークンの有効期間
             currentTimestamp : Math.floor(Date.now() / 1000)
-          };
-    
-          const privilegeExpiredTs = options.currentTimestamp + options.expirationTimeInSeconds;
-          const token = RtcTokenBuilder.buildTokenWithAccount(options.appId, options.appCertificate, options.channel, options.uid, RtcRole.PUBLISHER, privilegeExpiredTs);
-          
-          async function initializeRTC() {
+        },
+
+        this.rtc = {
+            localAudioTrack: null,
+            localVideoTrack: null,
+            client: null
+        }
+    }
+
+    VideoStreaming(){
+        let options:any = this.options;
+        let rtc:any = this.rtc;
+
+        const privilegeExpiredTs = options.currentTimestamp + options.expirationTimeInSeconds;
+        const token = RtcTokenBuilder.buildTokenWithAccount(options.appId, options.appCertificate, options.channel, options.uid, RtcRole.PUBLISHER, privilegeExpiredTs);
+        
+        async function initializeRTC() {
             rtc.client = AgoraRTC.createClient({ mode: "live", codec: "vp8" }) as IAgoraRTCClient;
             await rtc.client.join(options.appId, options.channel, token, options.uid);
             rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
             rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
             rtc.localVideoTrack.play("local_video");
             await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
-          }
-          initializeRTC();
-    }
+        }
+        initializeRTC();
+    };
+
+    RadioStreaming(){
+        let options:any = this.options;
+        let rtc:any = this.rtc;
+
+        const privilegeExpiredTs = options.currentTimestamp + options.expirationTimeInSeconds;
+        const token = RtcTokenBuilder.buildTokenWithAccount(options.appId, options.appCertificate, options.channel, options.uid, RtcRole.PUBLISHER, privilegeExpiredTs);
+
+        async function initializeRTC() {
+        rtc.client = AgoraRTC.createClient({ mode: "live", codec: "vp8" }) as IAgoraRTCClient;
+        await rtc.client.join(options.appId, options.channel, token, options.uid);
+        rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+        rtc.localAudioTrack.play("local_video");
+        await rtc.client.publish([rtc.localAudioTrack]); // await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
+        }
+        initializeRTC();
+    };
+
+    VideoLocalPreview(){
+        let rtc:any = this.rtc;
+        async function initializeRTC() {
+            rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+            rtc.localVideoTrack.play("local_video");
+        }
+        initializeRTC();
+    };
 }
 
 export default new AgoraLiveStreaming();
